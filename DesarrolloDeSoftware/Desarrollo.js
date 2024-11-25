4756da8fc576d941afa066f5b9194d659a1a8b0c
Santiago Torregrosa <santiago.torre07a@gmail.com>
2024-11-25 09:40:29 +0100

    boton js

--- DesarrolloDeSoftware/Desarrollo.js
+++ DesarrolloDeSoftware/Desarrollo.js
@@ -18,3 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+// Obtener el botón
+var botonSubir = document.getElementById("botonSubir");
+
+// Mostrar el botón cuando se hace scroll
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop
+        > 20) {
+        botonSubir.style.display = "block";
+    } else {
+        botonSubir.style.display = "none";
+
+    }
+};
+
+// Subir al hacer clic
+botonSubir.onclick = function () {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera   
+
+};
